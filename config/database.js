e78b0db07512647ee6c0bb10c975920627938b94
ishwant <ishwant08@gmail.com>
2016-03-26 19:21:13 -0700

    db

--- config/database.js
+++ config/database.js
@@ -1,6 +1,6 @@
 // config/database.js
 module.exports = {
 
-    'url' : 'mongodb://localhost/Diabetik' // looks like mongodb://<user>:<pass>@mongo.onmodulus.net:27017/Mikha4ot
+    'url' : 'mongodb://ishwant:punjab143@ds028559.mlab.com:28559/diabetik' // looks like mongodb://<user>:<pass>@mongo.onmodulus.net:27017/Mikha4ot
 
 };
